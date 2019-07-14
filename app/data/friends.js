bb227736b3e1b844448a448d42a48313efa26d9c
Joe Kinney <joekinney53@gmail.com>
2019-07-14 17:04:59 -0500

    made home page and inerted Friends.js data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,195 @@
-var tableArray = [
-    {
-      customerName: "Ahmed",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
-    }
+var candidates = [
+  {
+    "name":"Ben",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        3,
+        4,
+        2,
+        4,
+        1,
+        5,
+        3,
+        5,
+        1,
+        3
+
+      ]
+  },
+
+  {
+    "name":"Jacob",
+    "photo":"http://img03.deviantart.net/1932/i/2006/306/b/9/random_people_series___1_by_straight_thugin.jpg",
+    "scores":[
+        4,
+        1,
+        5,
+        3,
+        5,
+        2,
+        4,
+        5,
+        1,
+        2
+
+      ]
+  },
+
+  {
+    "name":"David",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        4,
+        2,
+        5,
+        3,
+        4,
+        4,
+        1,
+        2,
+        3,
+        1
+
+      ]
+  },
+
+  {
+    "name":"Jackson",
+    "photo":"https://i.ytimg.com/vi/GmHVifB8_Z0/maxresdefault.jpg",
+    "scores":[
+        4,
+        4,
+        4,
+        5,
+        1,
+        1,
+        3,
+        1,
+        4,
+        3
+
+      ]
+  },
+
+  {
+    "name":"Khalil",
+    "photo":"https://www.pentaxforums.com/forums/attachments/12-post-your-photos/107658d1320130707-people-random-people-portraits-imgp0912.jpg",
+    "scores":[
+        4,
+        5,
+        4,
+        5,
+        1,
+        3,
+        4,
+        1,
+        2,
+        3
+
+      ]
+  },
+
+  {
+    "name":"Jessica",
+    "photo":"https://slism.com/wpsystem/wp-content/uploads/talk-to-random-people-005-positive-woman-smiling.jpg",
+    "scores":[
+        4,
+        5,
+        3,
+        1,
+        3,
+        5,
+        4,
+        2,
+        3,
+        4
+
+      ]
+  },
+
+  {
+    "name":"Katie",
+    "photo":"https://kindredgrace.com/wp-content/uploads/2011/10/ylcf-27.jpg",
+    "scores":[
+        5,
+        3,
+        4,
+        1,
+        4,
+        5,
+        3,
+        4,
+        5,
+        2
+
+      ]
+  },
+
+  {
+    "name":"Danielle",
+    "photo":"https://www.essence.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-29-at-1.10.23-PM.png",
+    "scores":[
+       2,
+       4,
+       3,
+       1,
+       5,
+       3,
+       4,
+       2,
+       5,
+       1
+
+      ]
+  },
+
+  {
+    "name":"Anna",
+    "photo":"http://www.chinadaily.com.cn/bizchina/img/attachement/jpg/site1/20160120/b083fe955a0f1809404d41.jpg",
+    "scores":[
+       5,
+       5,
+       3,
+       4,
+       1,
+       2,
+       2,
+       4,
+       1,
+       1
+
+      ]
+  },
+
+  {
+    "name":"Tess",
+    "photo":"http://theprosperouscoach.com/wp-content/uploads/2013/10/woman-thinking.jpg",
+    "scores":[
+        4,
+        5,
+        2,
+        3,
+        1,
+        1,
+        4,
+        4,
+        2,
+        5
+
+      ]
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = tableArray;
+  module.exports = candidates;
   
